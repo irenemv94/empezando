fb8b38175158b94f02762b5999dd11ef0e82c387
irenemv94 <36760235+irenemv94@users.noreply.github.com>
2018-02-23 13:31:39 +0100

    empezando

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>BLOG</title>
+    <link href="estilos.css" rel="atylesheet" type="text/cas" />
+</head>
+
+<body>
+    <div class="cabecera">
+    </div>
+    
+    <div class="cuerpo">
+        
+    </div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+<?php
+
+?>
